398828750b740ef74f2eda53fc488a88c1058ce5
Francis Lee <francis@u.nus.edu>
2020-04-29 15:59:47 +0800

    v1.1.8 update update mechanism

--- install-all.ps1
+++ install-all.ps1
@@ -1,4 +1,4 @@
-﻿<# v1.1.7
+﻿<# v1.1.8
 .Description
 This script installs the applications listed in msi_list.txt sequentially.
 Requires input to -Source parameter
@@ -29,7 +29,7 @@ Param(
 
 BEGIN {
 
-    $currentVersion = "1.1.7"
+    $currentVersion = "1.1.8"
     $currentVersionDate = "29/04/2020"
     Write-Host Hello there! This is the DPC software install script! -ForegroundColor Yellow
     Write-Host "Current version of the script is v$currentVersion last updated on $currentVersionDate." -ForegroundColor Yellow
